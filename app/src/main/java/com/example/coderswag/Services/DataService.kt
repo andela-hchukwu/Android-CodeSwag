2db5e2e5c03537f705035dfbf181dd4c96b6f34f
Henry Chukwu <henry.chukwu@andela.com>
2019-03-03 02:13:36 +0100

    product recycle adapter

--- app/src/main/java/com/example/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -6,13 +6,17 @@ import com.example.coderswag.Model.Product
 object DataService {
 
     val categories = listOf(
-        Category("Shirts", "shirtimage"),
-        Category("Hoodies", "hoodieimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
         Category("HATS", "hatimage"),
         Category("DIGITAL", "digitalgoodsimage")
     )
 
     val hats = listOf(
+        Product("Devslopes graphic beanie", "$18", "hat1"),
+        Product("Devslopes hat black", "$20", "hat2"),
+        Product("Devslopes hat white", "$22", "hat3"),
+        Product("Devslopes hat snapback", "$14", "hat4"),
         Product("Devslopes graphic beanie", "$18", "hat1"),
         Product("Devslopes hat black", "$20", "hat2"),
         Product("Devslopes hat white", "$22", "hat3"),
@@ -23,10 +27,19 @@ object DataService {
         Product("Devslopes hoodie gray", "$28", "hoodie1"),
         Product("Devslopes hoodie red", "$32", "hoodie2"),
         Product("Devslopes hoodie blue", "$28", "hoodie3"),
+        Product("Devslopes hoodie black", "$38", "hoodie4"),
+        Product("Devslopes hoodie gray", "$28", "hoodie1"),
+        Product("Devslopes hoodie red", "$32", "hoodie2"),
+        Product("Devslopes hoodie blue", "$28", "hoodie3"),
         Product("Devslopes hoodie black", "$38", "hoodie4")
     )
 
     val shirts = listOf(
+        Product("Devslopes shirt gray", "$18", "shirt1"),
+        Product("Devslopes logo shirt red", "$32", "shirt2"),
+        Product("Devslopes hustle", "$20", "shirt3"),
+        Product("Devslopes badge light gray", "$18", "shirt4"),
+        Product("Kickflip studios", "$18", "shirt5"),
         Product("Devslopes shirt gray", "$18", "shirt1"),
         Product("Devslopes logo shirt red", "$32", "shirt2"),
         Product("Devslopes hustle", "$20", "shirt3"),
